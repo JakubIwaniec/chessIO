11990f19c62ce58fc7bab48a5dda88b11b9d4f86
miloszgost1 <miloszgost@wp.pl>
2023-11-28 18:38:22 +0100

    Naniesienie poprawek do skryptu 'main.py' cz. 2 (Issue #6)

--- Chess/main.py
+++ Chess/main.py
@@ -2,103 +2,103 @@ class Chessboard:
     def __init__(self):
         self.board = [['' for _ in range(8)] for _ in range(8)]
 
-    @staticmethod
-    def startgame() -> None:
+    @classmethod
+    def startgame(cls, target_board) -> None:
         white_rookA = Rook('White', (0, 0))
-        chessboard.place_piece(white_rookA, 0, 0)
+        target_board.place_piece(white_rookA, 0, 0)
 
         white_knightA = Knight('White', (0, 1))
-        chessboard.place_piece(white_knightA, 0, 1)
+        target_board.place_piece(white_knightA, 0, 1)
 
         white_bishopB = Bishop('White', (0, 2))
-        chessboard.place_piece(white_bishopB, 0, 2)
+        target_board.place_piece(white_bishopB, 0, 2)
 
         white_queen = Queen('White', (0, 3))
-        chessboard.place_piece(white_queen, 0, 3)
+        target_board.place_piece(white_queen, 0, 3)
 
         white_king = King('White', (0, 4))
-        chessboard.place_piece(white_king, 0, 4)
+        target_board.place_piece(white_king, 0, 4)
 
         white_bishopW = Bishop('White', (0, 5))
-        chessboard.place_piece(white_bishopW, 0, 5)
+        target_board.place_piece(white_bishopW, 0, 5)
 
         white_knightB = Knight('White', (0, 6))
-        chessboard.place_piece(white_knightB, 0, 6)
+        target_board.place_piece(white_knightB, 0, 6)
 
         white_rookB = Rook('White', (0, 7))
-        chessboard.place_piece(white_rookB, 0, 7)
+        target_board.place_piece(white_rookB, 0, 7)
 
         white_pawnA = Pawn('White', (1, 0))
-        chessboard.place_piece(white_pawnA, 1, 0)
+        target_board.place_piece(white_pawnA, 1, 0)
 
         white_pawnB = Pawn('White', (1, 1))
-        chessboard.place_piece(white_pawnB, 1, 1)
+        target_board.place_piece(white_pawnB, 1, 1)
 
         white_pawnC = Pawn('White', (1, 2))
-        chessboard.place_piece(white_pawnC, 1, 2)
+        target_board.place_piece(white_pawnC, 1, 2)
 
         white_pawnD = Pawn('White', (1, 3))
-        chessboard.place_piece(white_pawnD, 1, 3)
+        target_board.place_piece(white_pawnD, 1, 3)
 
         white_pawnE = Pawn('White', (1, 4))
-        chessboard.place_piece(white_pawnE, 1, 4)
+        target_board.place_piece(white_pawnE, 1, 4)
 
         white_pawnF = Pawn('White', (1, 5))
-        chessboard.place_piece(white_pawnF, 1, 5)
+        target_board.place_piece(white_pawnF, 1, 5)
 
         white_pawnG = Pawn('White', (1, 6))
-        chessboard.place_piece(white_pawnG, 1, 6)
+        target_board.place_piece(white_pawnG, 1, 6)
 
         white_pawnH = Pawn('White', (1, 7))
-        chessboard.place_piece(white_pawnH, 1, 7)
+        target_board.place_piece(white_pawnH, 1, 7)
 
         black_rookA = Rook('Black', (7, 0))
-        chessboard.place_piece(black_rookA, 7, 0)
+        target_board.place_piece(black_rookA, 7, 0)
 
         black_knightA = Knight('Black', (7, 1))
-        chessboard.place_piece(black_knightA, 7, 1)
+        target_board.place_piece(black_knightA, 7, 1)
 
         black_bishopW = Bishop('Black', (7, 2))
-        chessboard.place_piece(black_bishopW, 7, 2)
+        target_board.place_piece(black_bishopW, 7, 2)
 
         black_queen = Queen('Black', (7, 3))
-        chessboard.place_piece(black_queen, 7, 3)
+        target_board.place_piece(black_queen, 7, 3)
 
         black_king = King('Black', (7, 4))
-        chessboard.place_piece(black_king, 7, 4)
+        target_board.place_piece(black_king, 7, 4)
 
         black_bishopB = Bishop('Black', (7, 5))
-        chessboard.place_piece(black_bishopB, 7, 5)
+        target_board.place_piece(black_bishopB, 7, 5)
 
         black_knightB = Knight('Black', (7, 6))
-        chessboard.place_piece(black_knightB, 7, 6)
+        target_board.place_piece(black_knightB, 7, 6)
 
         black_rookB = Rook('Black', (7, 7))
-        chessboard.place_piece(black_rookB, 7, 7)
+        target_board.place_piece(black_rookB, 7, 7)
 
         black_pawnA = Pawn('Black', (6, 0))
-        chessboard.place_piece(black_pawnA, 6, 0)
+        target_board.place_piece(black_pawnA, 6, 0)
 
         black_pawnB = Pawn('Black', (6, 1))
-        chessboard.place_piece(black_pawnB, 6, 1)
+        target_board.place_piece(black_pawnB, 6, 1)
 
         black_pawnC = Pawn('Black', (6, 2))
-        chessboard.place_piece(black_pawnC, 6, 2)
+        target_board.place_piece(black_pawnC, 6, 2)
 
         black_pawnD = Pawn('Black', (6, 3))
-        chessboard.place_piece(black_pawnD, 6, 3)
+        target_board.place_piece(black_pawnD, 6, 3)
 
         black_pawnE = Pawn('Black', (6, 4))
-        chessboard.place_piece(black_pawnE, 6, 4)
+        target_board.place_piece(black_pawnE, 6, 4)
 
         black_pawnF = Pawn('Black', (6, 5))
-        chessboard.place_piece(black_pawnF, 6, 5)
+        target_board.place_piece(black_pawnF, 6, 5)
 
         black_pawnG = Pawn('Black', (6, 6))
-        chessboard.place_piece(black_pawnG, 6, 6)
+        target_board.place_piece(black_pawnG, 6, 6)
 
         black_pawnH = Pawn('Black', (6, 7))
-        chessboard.place_piece(black_pawnH, 6, 7)
+        target_board.place_piece(black_pawnH, 6, 7)
 
     def print_board(self):
         for row in self.board:
@@ -322,7 +322,7 @@ class King(Piece):
 # Użycie klas
 if __name__ == '__main__':
     chessboard = Chessboard()
-    chessboard.startgame()
+    Chessboard.startgame(chessboard)
     chessboard.print_board()
     is_white_turn = True
     en_passant = None
