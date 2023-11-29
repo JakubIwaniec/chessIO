97ae0fb6fadce4c2a6f29d7da907a8ef6753f65e
Iwan <jakub.iwaniec3@gmail.com>
2023-11-29 01:56:52 +0100

    stworzenie pliku Piece.py, przeniesienie do niego klas figur, dodanie ścieżki do grafik dla każdej figury oraz mniejsze poprawki

--- Chess/main.py
+++ Chess/main.py
@@ -1,104 +1,65 @@
+from Piece import *
 class Chessboard:
     def __init__(self):
         self.board = [['' for _ in range(8)] for _ in range(8)]
 
     @classmethod
     def startgame(cls, target_board) -> None:
-        white_rookA = Rook('White', (0, 0))
+        white_rookA = Rook('White')
         target_board.place_piece(white_rookA, 0, 0)
 
-        white_knightA = Knight('White', (0, 1))
+        white_knightA = Knight('White')
         target_board.place_piece(white_knightA, 0, 1)
 
-        white_bishopB = Bishop('White', (0, 2))
+        white_bishopB = Bishop('White')
         target_board.place_piece(white_bishopB, 0, 2)
 
-        white_queen = Queen('White', (0, 3))
+        white_queen = Queen('White')
         target_board.place_piece(white_queen, 0, 3)
 
-        white_king = King('White', (0, 4))
+        white_king = King('White')
         target_board.place_piece(white_king, 0, 4)
 
-        white_bishopW = Bishop('White', (0, 5))
+        white_bishopW = Bishop('White')
         target_board.place_piece(white_bishopW, 0, 5)
 
-        white_knightB = Knight('White', (0, 6))
+        white_knightB = Knight('White')
         target_board.place_piece(white_knightB, 0, 6)
 
-        white_rookB = Rook('White', (0, 7))
+        white_rookB = Rook('White')
         target_board.place_piece(white_rookB, 0, 7)
 
-        white_pawnA = Pawn('White', (1, 0))
-        target_board.place_piece(white_pawnA, 1, 0)
+        for x in range(8):
+            white_pawn = Pawn('White')
+            target_board.place_piece(white_pawn, 1, x)
 
-        white_pawnB = Pawn('White', (1, 1))
-        target_board.place_piece(white_pawnB, 1, 1)
+        for x in range(8):
+            black_pawn = Pawn('Black')
+            target_board.place_piece(black_pawn, 6, x)
 
-        white_pawnC = Pawn('White', (1, 2))
-        target_board.place_piece(white_pawnC, 1, 2)
-
-        white_pawnD = Pawn('White', (1, 3))
-        target_board.place_piece(white_pawnD, 1, 3)
-
-        white_pawnE = Pawn('White', (1, 4))
-        target_board.place_piece(white_pawnE, 1, 4)
-
-        white_pawnF = Pawn('White', (1, 5))
-        target_board.place_piece(white_pawnF, 1, 5)
-
-        white_pawnG = Pawn('White', (1, 6))
-        target_board.place_piece(white_pawnG, 1, 6)
-
-        white_pawnH = Pawn('White', (1, 7))
-        target_board.place_piece(white_pawnH, 1, 7)
-
-        black_rookA = Rook('Black', (7, 0))
+        black_rookA = Rook('Black')
         target_board.place_piece(black_rookA, 7, 0)
 
-        black_knightA = Knight('Black', (7, 1))
+        black_knightA = Knight('Black')
         target_board.place_piece(black_knightA, 7, 1)
 
-        black_bishopW = Bishop('Black', (7, 2))
+        black_bishopW = Bishop('Black')
         target_board.place_piece(black_bishopW, 7, 2)
 
-        black_queen = Queen('Black', (7, 3))
+        black_queen = Queen('Black')
         target_board.place_piece(black_queen, 7, 3)
 
-        black_king = King('Black', (7, 4))
+        black_king = King('Black')
         target_board.place_piece(black_king, 7, 4)
 
-        black_bishopB = Bishop('Black', (7, 5))
+        black_bishopB = Bishop('Black')
         target_board.place_piece(black_bishopB, 7, 5)
 
-        black_knightB = Knight('Black', (7, 6))
+        black_knightB = Knight('Black')
         target_board.place_piece(black_knightB, 7, 6)
 
-        black_rookB = Rook('Black', (7, 7))
+        black_rookB = Rook('Black')
         target_board.place_piece(black_rookB, 7, 7)
-
-        black_pawnA = Pawn('Black', (6, 0))
-        target_board.place_piece(black_pawnA, 6, 0)
-
-        black_pawnB = Pawn('Black', (6, 1))
-        target_board.place_piece(black_pawnB, 6, 1)
-
-        black_pawnC = Pawn('Black', (6, 2))
-        target_board.place_piece(black_pawnC, 6, 2)
-
-        black_pawnD = Pawn('Black', (6, 3))
-        target_board.place_piece(black_pawnD, 6, 3)
-
-        black_pawnE = Pawn('Black', (6, 4))
-        target_board.place_piece(black_pawnE, 6, 4)
-
-        black_pawnF = Pawn('Black', (6, 5))
-        target_board.place_piece(black_pawnF, 6, 5)
-
-        black_pawnG = Pawn('Black', (6, 6))
-        target_board.place_piece(black_pawnG, 6, 6)
-
-        black_pawnH = Pawn('Black', (6, 7))
-        target_board.place_piece(black_pawnH, 6, 7)
 
     def print_board(self):
         for row in self.board:
@@ -225,105 +186,7 @@ class Chessboard:
             diff_row = abs(end_row - start_row)
             diff_col = abs(end_col - start_col)
             return diff_row <= 1 and diff_col <= 1, None
-        # Dodaj logikę dla innych pionków
         return False, None
-
-
-class Piece:
-    def __init__(self, color, position):
-        self.color = color
-        self.position = position
-        self.path_to_image = None
-
-
-class Pawn(Piece):
-    # wzór do skopiowania (Ułaniec masz)
-    def __init__(self, color, position):
-        super().__init__(color, position)
-        if super().color == 'White':
-            # ! inny plik w zaleznosci od podklasy i koloru
-            super().path_to_image = 'Images\\FigureSkins\\wP.jpg'
-        elif super().color == 'Black':
-            super().path_to_image = 'Images\\FigureSkins\\bP.jpg'
-
-    def __repr__(self):
-        if self.color == 'White':
-            return 'p'
-        elif self.color == 'Black':
-            return 'P'
-
-    def __str__(self):
-        return 'Pawn'
-
-
-class Rook(Piece):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-
-    def __repr__(self):
-        if self.color == 'White':
-            return 'r'
-        elif self.color == 'Black':
-            return 'R'
-
-    def __str__(self):
-        return 'Rook'
-
-
-class Knight(Piece):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-
-    def __repr__(self):
-        if self.color == 'White':
-            return 'n'
-        elif self.color == 'Black':
-            return 'N'
-
-    def __str__(self):
-        return 'Knight'
-
-
-class Bishop(Piece):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-
-    def __repr__(self):
-        if self.color == 'White':
-            return 'b'
-        elif self.color == 'Black':
-            return 'B'
-
-    def __str__(self):
-        return 'Bishop'
-
-
-class Queen(Piece):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-
-    def __repr__(self):
-        if self.color == 'White':
-            return 'q'
-        elif self.color == 'Black':
-            return 'Q'
-
-    def __str__(self):
-        return 'Queen'
-
-
-class King(Piece):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-
-    def __repr__(self):
-        if self.color == 'White':
-            return 'k'
-        elif self.color == 'Black':
-            return 'K'
-
-    def __str__(self):
-        return 'King'
 
 
 # Użycie klas
